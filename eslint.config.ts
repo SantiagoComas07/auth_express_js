b245a4d3f154d56ad677b3341c85fae4b13ac4aa
SantiagoComas07 <santiagocomas07d@gmail.com>
2025-12-21 19:11:52 -0500

    eslint removed

--- eslint.config.ts
+++
@@ -1,9 +0,0 @@
-
-import globals from "globals";
-import tseslint from "typescript-eslint";
-import { defineConfig } from "eslint/config";
-
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], extends: ["js/recommended"], languageOptions: { globals: {...globals.browser,...globals.node} } },
-  tseslint.configs.recommended,
-]);
